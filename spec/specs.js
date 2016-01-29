e7115ef22a24fcb17dd694e96735d591a33f49be
Pablo Vera <pablovera.pdx@gmail.com>
2016-01-29 11:50:52 -0800

    add second specs. Not tested yet

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('Pizza', function () {
-  it("will make a pizza with according to what the costumer wants ", function () {
-    var testPizza = new Pizza("1", "small", "pepperoni", "5");
+  it("will make a pizza based on what the costumer wants", function () {
+    var testPizza = new Pizza("1", "small", "pepperoni", 5);
     expect(testPizza.pizzaQuantity).to.equal("1");
     expect(testPizza.pizzaSize).to.equal("small");
     expect(testPizza.pizzaTopping).to.equal("pepperoni");
-    expect(testPizza.pizzaPrice).to.equal("5");
+    expect(testPizza.pizzaPrice).to.equal(5);
+  });
+  it("adds price method to constructor", function() {
+  var testPizza = new Pizza("1", "small", "mushrooms");
+  expect(testOrder.calculatePrice()).to.equal(5);
   });
 });
