7a53df65a87eebdc901a81da5d13c755c05d7bc1
Pablo Vera <pablovera.pdx@gmail.com>
2016-01-29 09:35:52 -0800

    first spec tested

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function Pizza (pizzaQuantity, pizzaSize, pizzaTopping, pizzaPrice) {
+  this.pizzaQuantity = pizzaQuantity;
+  this.pizzaSize = pizzaSize;
+  this.pizzaTopping = pizzaTopping;
+  this.pizzaPrice = pizzaPrice
+}
