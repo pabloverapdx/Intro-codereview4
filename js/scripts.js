8ebd97547939753a03ab43579469cd454a2ae5eb
Pablo Vera <pablovera.pdx@gmail.com>
2016-02-04 09:42:35 -0800

    add passing test on second spec for total price of customized order

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,37 @@ function Pizza(size, toppings, quantity) {
   this.toppings = toppings;
   this.quantity = quantity;
 }
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+};
+
+Pizza.prototype.totalPrice = function() {
+  var sizePrice = 0;
+
+  if (this.size === "X-Large") {
+      sizePrice += 20;
+  } else if (this.size === "Large") {
+      sizePrice += 16;
+  } else if (this.size === "Medium") {
+      sizePrice += 12;
+  } else if (this.size === "Small") {
+      sizePrice += 8;
+  }
+
+  for(var i=0; i<this.toppings.length; i++) {
+    if (this.toppings[i] === "Chicken" || this.toppings[i] === "Pepperoni" || this.toppings[i] === "Sausage" || this.toppings[i] === "Bacon" || this.toppings[i] === "Salami" || this.toppings[i] === "Ham") {
+        sizePrice += 2;
+    }  else if (this.toppings[i] === "Green Pepper" || this.toppings[i] === "Mushroom" || this.toppings[i] === "Onion" || this.toppings[i] === "Black Olive") {
+        sizePrice += 1;
+    }
+  }
+
+
+  var totalPrice = sizePrice * this.quantity;
+
+  return totalPrice;
+  }
 // $(document).ready(function() {
 //   $("form#letsMakePizza").submit(function(event) {
 // event.preventDefault();
