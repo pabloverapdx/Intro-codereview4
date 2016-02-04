045b9bcbcb9dbed365745dbce9c3443895dd6576
Pablo Vera <pablovera.pdx@gmail.com>
2016-02-04 09:23:30 -0800

    add spec that calculates the total price of customized order

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,10 @@ describe("Pizza", function() {
     expect(testPizza.toppings).to.eql([]);
     expect(testPizza.quantity).to.equal(1);
   });
+
+it('calculates the total price of the customized order', function() {
+  var testPizza = new Pizza("Large", [], 1);
+  testPizza.addTopping("Pepperoni");
+  expect(testPizza.totalPrice()).to.equal(18);
+  });
 });
